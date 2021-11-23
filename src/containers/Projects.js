1d6d2177fe3da06345a083d041a7f2ef108bc4c2
Khallekan <okeolalekanisaac@gmail.com>
2021-11-23 18:26:52 +0100

    First Deployment v1.0

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useGlobalContext } from "../context";
+import Socials from "../components/Socials";
 
 const Projects = () => {
   const { dispatch } = useGlobalContext();
@@ -7,7 +8,16 @@ const Projects = () => {
   useEffect(() => {
     dispatch({ type: `HANDLE_MODAL`, payload: false });
   }, [dispatch]);
-  return <div>Projects</div>;
+  return (
+    <div className={`mainapp-content_container`}>
+      <section className='section1 flex items-center justify-center'>
+        PROJECTS COMING SOON ...
+      </section>
+      <section className='section2 mainapp_home_container'>
+        <Socials className={`mainapp_home-socials`} />
+      </section>
+    </div>
+  );
 };
 
 export default Projects;
